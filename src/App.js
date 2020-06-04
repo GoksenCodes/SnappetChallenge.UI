9962f1d1b185aade3f37d3215b0d4808113fbf54
Goksen <goksengorgulu@gmail.com>
2020-06-04 23:10:50 +0200

    set up mixins&functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hello from react aaaa </div>;
+  return (
+    <div className="App">
+      <div className="container"></div>
+    </div>
+  );
 }
 
 export default App;
