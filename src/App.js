a2d34ca3df053afe08660f7b5863eb99e25e3fa7
Goksen <goksengorgulu@gmail.com>
2020-06-08 00:40:04 +0200

    final build & commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.scss";
 import { Switch, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navigation from "./components/navigation";
 import List from "./components/List";
 
 function App() {
