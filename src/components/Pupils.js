1870280d78d9521bf0870f64bb19a0efa19bf91d
Goksen <goksengorgulu@gmail.com>
2020-06-12 13:07:42 +0200

    resolve unique key warning for spans for pupils

--- src/components/Pupils.js
+++ src/components/Pupils.js
@@ -5,18 +5,28 @@ export default function Pupils(props) {
   const inprogressPupils = props.inprogress;
   const notstartedPupils = props.notstarted;
 
+  let index = 0;
   const pupils = [];
   for (let i = 0; i < completedPupils; i++) {
-    pupils.push(<span className="pupil pupil__completed"></span>);
+    pupils.push(<span key={index} className="pupil pupil__completed"></span>);
+    index++;
   }
 
   for (let i = 0; i < inprogressPupils; i++) {
-    pupils.push(<span className="pupil pupil__inprogress"></span>);
+    pupils.push(<span key={index} className="pupil pupil__inprogress"></span>);
+    index++;
   }
 
   for (let i = 0; i < notstartedPupils; i++) {
-    pupils.push(<span className="pupil pupil__notstarted"></span>);
+    pupils.push(<span key={index} className="pupil pupil__notstarted"></span>);
+    index++;
   }
 
   return <div>{pupils.map(a => a)}</div>;
 }
+
+// {uniqueGrades.map(uniqueGrade => {
+//   return (
+//     <option value={uniqueGrade} key={uniqueGrade} selected>
+//       Math Grade {uniqueGrade}
+//     </option>
